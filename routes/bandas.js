b9782b59eca99d570ddfe7e6f95700af4c65e2e6
bautipeleritti <bpeleritti@udesa.edu.ar>
2024-03-25 12:46:44 -0300

    hipervínculos

--- routes/bandas.js
+++ routes/bandas.js
@@ -4,9 +4,7 @@ const router= express.Router();
 const db = require('../db/index');
 const bandasController = require('../controllers/bandasController');
 
-//router.get('/', function(req,res) {
-  //  return res.send(db.lista)
-//})
+
 router.get('/', bandasController.index)
 router.get('/id/:id', bandasController.filtrarPorId)
 router.get('/genero/:genero', bandasController.filtrarPorGenero)
